05cd0e512ac0853cb0204e6742bdb6df046daed7
vihdutta <vihdutta@gmail.com>
2022-01-12 20:43:09 -0500

    fixed img refreshing

--- src/main.go
+++ src/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -15,33 +13,17 @@ import (
 var path = "C:\\Users\\Duttas\\Desktop\\LR Exports\\DSC_0005.JPG"
 
 func main() {
-	go logic()
-	go test_print()
-
 	a := app.New()
 	w := a.NewWindow("Auto Screenshot Opener")
 	w.Resize(fyne.NewSize(500, 500))
 
 	img := canvas.NewImageFromFile(path)
-	go func() {
-		for {
-			img.File = path
-			img.Refresh()
-			w.SetContent(img)
-		}
-	}()
-	w.SetContent(img)
+
+	go logic(img, w)
 	w.ShowAndRun()
 }
 
-func test_print() {
-	for {
-		time.After(time.Second)
-		fmt.Println(path)
-	}
-}
-
-func logic() {
+func logic(img *canvas.Image, w fyne.Window) {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
@@ -65,6 +47,9 @@ func logic() {
 				if event.Op&fsnotify.Write == fsnotify.Write {
 					log.Println(event.Name)
 					path = event.Name
+					img.File = path
+					img.Refresh()
+					w.SetContent(img)
 				}
 			case err, ok := <-watcher.Errors:
 				if !ok {
